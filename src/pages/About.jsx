a5ee64d6b8508545cdaa21b89789607ddc5820fa
Gal Luski <109075193+galuski@users.noreply.github.com>
2026-07-16 15:39:28 +0300

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,17 +7,16 @@ export function About() {
     const { t, ready } = useTranslation();
 
     return (
+
         <section className="About">
             <img className="about-img animate__animated animate__fadeIn animate__delay-1s" src={aboutIMG} alt={t("About us image")} />
-
             <div className="animate__animated animate__fadeIn animate__delay-2s">
-            <Title title={t("About")} orientation="horizontal" aria-label={t("About page title")} />
+                <Title title={t("About")} orientation="horizontal" aria-label={t("About page title")} />
             </div>
 
             <pre className="animate__animated animate__fadeIn animate__delay-3s">
                 {t(`About-Text`)}
             </pre>
-
         </section>
     )
 }
