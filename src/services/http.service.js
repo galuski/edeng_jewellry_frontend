38089cc7c1c0ba4426561bb969d2a42998521365
Gal Luski <109075193+galuski@users.noreply.github.com>
2026-07-16 15:53:05 +0300

    Fix API URL for Vite

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 // console.log(process.env.NODE_ENV);
 
 
-const BASE_URL = process.env.NODE_ENV === 'production'
+const BASE_URL = import.meta.env.PROD
     ? '/api/'
     : '//localhost:3030/api/'
 
