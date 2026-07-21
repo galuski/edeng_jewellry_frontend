4721d99ef6662c6e20d3f42410c97751ae9d4170
Gal Luski <109075193+galuski@users.noreply.github.com>
2026-07-21 14:55:01 +0300

    fix coupon

--- src/cmps/CouponPopup.jsx
+++ src/cmps/CouponPopup.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from "react-i18next";
-import './CouponPopup.css'; // נוצר בשלב הבא
 
 export function CouponPopup({ isOpen, onClose, discountAmount, currency }) {
     const { t } = useTranslation();
